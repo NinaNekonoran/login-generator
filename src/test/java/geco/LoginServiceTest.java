d08888b41ef76551b43271790d7130824802e214
NinaNekonoran <franzese.rozen@gmail.com>
2019-11-19 09:02:04 +0100

    close #1 add all test in LoginServiceTest and full covered

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -3,27 +3,46 @@ package geco;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
-
+    String[] loginsExistants = {"coucou","non"};
+    LoginService ls = null;
     @Before
     public void setUp() throws Exception {
+        String[] loginsExistants = {"coucou","non"};
+        ls = new LoginService(loginsExistants);
     }
 
     @Test
     public void loginExists() {
+        assertTrue(ls.loginExists("coucou"));
+        assertFalse(ls.loginExists("qsfqsdfqs"));
     }
 
     @Test
     public void addLogin() {
+        assertFalse(ls.loginExists("Bonjour"));
+        ls.addLogin("Bonjour");
+        assertTrue(ls.loginExists("Bonjour"));
     }
 
     @Test
     public void findAllLoginsStartingWith() {
+        List<String> resultats = new ArrayList<>();
+        resultats.add("coucou");
+        assertTrue(ls.findAllLoginsStartingWith("cou") instanceof List);
+        assertEquals(resultats, ls.findAllLoginsStartingWith("cou"));
     }
 
     @Test
     public void findAllLogins() {
+        List<String> resultats = new ArrayList<>();
+        resultats.add(loginsExistants[0]);
+        resultats.add(loginsExistants[1]);
+        assertEquals(ls.findAllLogins(),resultats);
     }
 }
